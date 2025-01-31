8c44d31527b967d53f03ac0f0d8f59c01b195ddb
Deepak-Dev-4656 <developerdev4656@gmail.com>
2025-01-31 22:56:36 +0530

    Update HeroSection.js

--- src/pages/HeroSection.js
+++ src/pages/HeroSection.js
@@ -15,12 +15,7 @@ const HeroSection = () => {
       <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
 
       {/* Hero Content */}
-      <div className="relative z-10 text-center px-6">
-       
-        <button className="px-6 py-3 bg-gradient-to-r from-cyan-500 to-blue-600 rounded-full text-white font-semibold shadow-lg transform transition-all duration-300 hover:scale-105 hover:shadow-xl">
-          Get Started
-        </button>
-      </div>
+      
     </div>
   );
 };
